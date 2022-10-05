b0ea8f5d1b3e679c6d2ac9d4932ad32f49759f23
unknown <memogarcia246@gmail.com>
2022-10-05 13:34:24 -0500

    started with semantic functionalities of semantic cube and function directories

---
+++ semantics/semanticCube.js
@@ -0,0 +1,143 @@
+// semanticCube.js
+// Semantic "cube" that determines the result type of an operator applied to 2 operands
+
+// Access to an entry in semCube looks like: 
+// semCube["operand1"]["operand2"]["operator"]
+const semCube = {
+    int: {
+        int: {
+            "+": "int",
+            "-": "int",
+            "*": "int",
+            "/": "int",
+            ">=": "",
+            ">": "",
+            "<=": "",
+            "<": "",
+            "==": "",
+            "!=": "",
+            "&": "error",
+            "|": "error",
+        },
+        float: {
+            "+": "float",
+            "-": "float",
+            "*": "float",
+            "/": "float",
+            ">=": "",
+            ">": "",
+            "<=": "",
+            "<": "",
+            "==": "",
+            "!=": "",
+            "&": "error",
+            "|": "error",
+        },
+        char: {
+            "+": "error",
+            "-": "error",
+            "*": "error",
+            "/": "error",
+            ">=": "error",
+            ">": "error",
+            "<=": "error",
+            "<": "error",
+            "==": "error",
+            "!=": "error",
+            "&": "error",
+            "|": "error",
+        }
+    },
+    float: {
+        int: {
+            "+": "float",
+            "-": "float",
+            "*": "float",
+            "/": "float",
+            ">=": "",
+            ">": "",
+            "<=": "",
+            "<": "",
+            "==": "",
+            "!=": "",
+            "&": "error",
+            "|": "error",
+        },
+        float: {
+            "+": "float",
+            "-": "float",
+            "*": "float",
+            "/": "float",
+            ">=": "",
+            ">": "",
+            "<=": "",
+            "<": "",
+            "==": "",
+            "!=": "",
+            "&": "error",
+            "|": "error",
+        },
+        char: {
+            "+": "error",
+            "-": "error",
+            "*": "error",
+            "/": "error",
+            ">=": "error",
+            ">": "error",
+            "<=": "error",
+            "<": "error",
+            "==": "error",
+            "!=": "error",
+            "&": "error",
+            "|": "error",
+        }
+    },
+    char: {
+        int: {
+            "+": "error",
+            "-": "error",
+            "*": "error",
+            "/": "error",
+            ">=": "error",
+            ">": "error",
+            "<=": "error",
+            "<": "error",
+            "==": "error",
+            "!=": "error",
+            "&": "error",
+            "|": "error",
+        },
+        float: {
+            "+": "error",
+            "-": "error",
+            "*": "error",
+            "/": "error",
+            ">=": "error",
+            ">": "error",
+            "<=": "error",
+            "<": "error",
+            "==": "error",
+            "!=": "error",
+            "&": "error",
+            "|": "error",
+        },
+        char: {
+            "+": "error",
+            "-": "error",
+            "*": "error",
+            "/": "error",
+            ">=": "error",
+            ">": "error",
+            "<=": "error",
+            "<": "error",
+            "==": "",
+            "!=": "",
+            "&": "error",
+            "|": "error",
+        }
+    }
+}
+
+
+const o1 = "int", o2 = "float", op = "*"
+console.log(semCube[o1][o2][op])
